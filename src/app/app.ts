39d717e2c8640e2ebaff298b23195a05c1a16cc4
matheusdias05 <diasm9544@gmail.com>
2025-06-20 20:59:42 -0300

    feat: tela de login com validacao basica

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {  Login } from './login/login';
+import { LoginComponent } from './login/login';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Login],
+  imports: [LoginComponent],
   template: `<app-login></app-login>`,
 })
 export class App {}
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
-export class Login {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required]]
+    });
+  }
+
+  login() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const { email, senha } = this.form.value;
+    console.log('Login com:', email, senha);
+    // Aqui você pode redirecionar ou autenticar
+  }
 }
